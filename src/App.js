a1e1067163c618a2c833bb59629b58982d257544
Bayu Utomo <by.ciikel@gmail.com>
2020-01-16 21:04:01 +0700

    Update UI: style

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React from 'react'
-import { css } from 'emotion'
 
 import '@/css/tailwind.css'
-import '@/css/background.css'
+import '@/css/style.css'
 
 function App() {
   return (
     <div>
-      <div className={css`
-        -webkit-text-stroke-width: 2px;
-        -webkit-text-stroke-color: #283593;`
-        + 
-        " w-5/6 sm:w-2/4 lg:w-1/3 mx-auto pt-6 border-b-2 border-red-600 text-center text-5xl text-yellow-500 font-black subpixel-antialiased"}
-      >
+      <div className="custom-title w-5/6 sm:w-2/4 lg:w-1/3 mx-auto pt-6 border-b-2 border-red-600 text-center text-5xl text-yellow-500 font-black subpixel-antialiased">
         POKEDEX APP
       </div>
       <div className="p-2 text-center text-lg text-gray-700 antialiased">
