9d852be1f4ac7174b5ede7754eab58dd58df1d9a
Bayu Utomo <by.ciikel@gmail.com>
2020-01-16 19:21:36 +0700

    Add UI: header section

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import '@/css/tailwind.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div class="w-1/6 mx-auto pt-6 border-b-2 border-red-600 text-center text-2xl text-orange-300 font-black subpixel-antialiased">POKODEX APP</div>
+      <div class="p-2 text-center text-lg text-gray-700">Select your favorite pokemon partner.</div>
+  
+      <nav class="flex justify-center flex-wrap mt-4 mb-8 text-white">
+        <div class="inline-block bg-gray-200 hover:bg-red-600 hover:text-white rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 cursor-pointer">#grass</div>
+      </nav>
+
     </div>
-  );
+  )
 }
 
 export default App;
