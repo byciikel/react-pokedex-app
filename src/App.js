55fc73054d2c4d85801a56cf5be35a54bc0f0391
Bayu Utomo <by.ciikel@gmail.com>
2020-01-16 23:39:04 +0700

    Add package infinite scroll

--- src/App.js
+++ src/App.js
@@ -32,17 +32,14 @@ class App extends Component {
         <div className="flex justify-center flex-wrap p-10">
           {Store.dataAllPokemon ? 
             Store.dataAllPokemon.map((pokemon, index) => (
-              <div key={index} className="flex-initial w-48 rounded overflow-hidden shadow-lg bg-white hover:bg-gray-200 m-5 cursor-pointer">
-                <img className="w-auto mx-auto" src={pokemon.sprites.front_default} alt={pokemon.name} />
-                <div className="px-4 py-2 pb-0">
-                <div className="font-bold text-sm mb-2 text-center">{pokemon.name}</div>
+              <div key={index} className="flex-initial w-48 rounded-lg overflow-hidden shadow-lg bg-white hover:bg-gray-200 m-5 cursor-pointer">
+                <img className="w-auto mx-auto pt-3" src={pokemon.sprites.front_default} alt={pokemon.name} />
+                <div className="px-4 py-0">
+                <div className="font-bold text-sm mb-2 text-center capitalize">{pokemon.name}</div>
                 </div>
-                <div className="px-4 py-2 pt-0 text-center">
+                <div className="px-4 py-3 pt-0 text-center">
                   {pokemon.types.map((type, index) => (
-                  <span key={index} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-xs font-semibold text-gray-700 m-1">
-                    #
-                    {type.type.name}
-                  </span>
+                    <span key={index} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-xs font-semibold text-gray-700 m-1">#{type.type.name}</span>
                   ))}
                 </div>
               </div>
