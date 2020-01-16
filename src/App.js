099f1a0bd020e17ae472ee0fc31a7f523c5e5ef4
Bayu Utomo <by.ciikel@gmail.com>
2020-01-16 21:19:55 +0700

    Add fetch all pokemon with 100 limit

--- src/App.js
+++ src/App.js
@@ -1,42 +1,49 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { observer } from 'mobx-react'
+import Store from './Store'
 
 import '@/css/tailwind.css'
 import '@/css/style.css'
 
-function App() {
-  return (
-    <div>
-      <div className="custom-title w-5/6 sm:w-2/4 lg:w-1/3 mx-auto pt-6 border-b-2 border-red-600 text-center text-5xl text-yellow-500 font-black subpixel-antialiased">
-        POKEDEX APP
-      </div>
-      <div className="p-2 text-center text-lg text-gray-700 antialiased">
-        Select your favorite pokemon partner.
-      </div>
-  
-      <nav className="flex justify-center flex-wrap mt-4 mb-8 text-white">
-        <div className="inline-block bg-gray-200 hover:bg-red-600 hover:text-white rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 cursor-pointer">
-          #grass
-        </div>
-        <div className="inline-block bg-gray-200 hover:bg-red-600 hover:text-white rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 cursor-pointer">
-          #fire
-        </div>
-      </nav>
+class App extends Component {
+  componentDidMount = () => {
+    Store.fetchAllPokemon()
+  }
 
-      <div className="flex justify-center flex-wrap p-10">
-        <div className="flex-initial w-48 rounded overflow-hidden shadow-lg bg-white hover:bg-gray-200 m-5 cursor-pointer">
-          <img className="w-auto mx-auto" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png" alt="Ditto" />
-          <div className="px-4 py-2 pb-0">
-            <div className="font-bold text-sm mb-2 text-center">Ditto</div>
+  render() {
+    return (
+      <div>
+        <div className="custom-title w-5/6 sm:w-2/4 lg:w-1/3 mx-auto pt-6 border-b-2 border-red-600 text-center text-5xl text-yellow-500 font-black subpixel-antialiased">
+          POKEDEX APP
+        </div>
+        <div className="p-2 text-center text-lg text-gray-700 antialiased">
+          Select your favorite pokemon partner.
+        </div>
+    
+        <nav className="flex justify-center flex-wrap mt-4 mb-8 text-white">
+          <div className="inline-block bg-gray-200 hover:bg-red-600 hover:text-white rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 cursor-pointer">
+            #grass
           </div>
-          <div className="px-4 py-2 pt-0 text-center">
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-xs font-semibold text-gray-700 m-1">#ground</span>
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-xs font-semibold text-gray-700 m-1">#grass</span>
+          <div className="inline-block bg-gray-200 hover:bg-red-600 hover:text-white rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 cursor-pointer">
+            #fire
+          </div>
+        </nav>
+
+        <div className="flex justify-center flex-wrap p-10">
+          <div className="flex-initial w-48 rounded overflow-hidden shadow-lg bg-white hover:bg-gray-200 m-5 cursor-pointer">
+            <img className="w-auto mx-auto" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png" alt="Ditto" />
+            <div className="px-4 py-2 pb-0">
+              <div className="font-bold text-sm mb-2 text-center">Ditto</div>
+            </div>
+            <div className="px-4 py-2 pt-0 text-center">
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-xs font-semibold text-gray-700 m-1">#ground</span>
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-xs font-semibold text-gray-700 m-1">#grass</span>
+            </div>
           </div>
         </div>
       </div>
-
-    </div>
-  )
+    )
+  }
 }
 
-export default App;
+export default observer(App)
--- src/Store.js
+++ src/Store.js
@@ -1,10 +1,29 @@
-import React from 'react'
 import { decorate, observable } from 'mobx'
 
 class Store {
+  fetchAllPokemon() {
+    fetch(`https://pokeapi.co/api/v2/pokemon?limit=100`, {
+      method: 'GET'
+    }).then(
+      res => res.json()
+    ).then(
+      (result) => {
+        this.setDataAllPokemon(result)
+      },
+      (error) => {
+        console.log(error)
+      }
+    )
+  }
+
+  setDataAllPokemon(datas) {
+    this.dataAllPokemon = datas
+  }
 }
 
 decorate(Store, {
+  fetchAllPokemon: observable,
+  dataAllPokemon: observable
 })
 
 const NewStore = new Store()
