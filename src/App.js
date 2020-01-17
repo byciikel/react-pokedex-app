8eff06f0faae78834e88b337dff1280c2da83a7a
Bayu Utomo <by.ciikel@gmail.com>
2020-01-18 01:36:24 +0700

    Update structure code

--- src/App.js
+++ src/App.js
@@ -9,10 +9,51 @@ import '@/assets/css/tailwind.css'
 import '@/assets/css/style.css'
 
 class PokemonCard extends Component {
+  render() {
+    return (
+      <div className="flex justify-center flex-wrap lg:w-5/6 mx-auto p-10 py-0">
+        {Store.dataAllPokemon ? 
+          Store.dataAllPokemon.map((pokemon, index) => (
+            <div key={index} className="flex-initial w-48 rounded-lg overflow-hidden shadow-lg bg-white hover:bg-gray-200 m-5 cursor-pointer">
+              <div
+                className={css`
+                  background-image: url(${pokemon.sprites.front_default});
+                  background-repeat: no-repeat;
+                  background-position: center; `
+                  + " w-48 h-32 mx-auto pt-3"
+                }
+              />
+              <div className="px-4 py-0">
+              <div className="font-bold text-sm mb-2 text-center capitalize">{pokemon.name.replace('-', ' ')}</div>
+              </div>
+              <div className="px-4 py-3 pt-0 text-center">
+                {pokemon.types.map((type, index) => (
+                  <span key={index} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-xs font-semibold text-gray-700 m-1">#{type.type.name}</span>
+                ))}
+              </div>
+            </div>
+          ))
+          : <div/>
+        }
+      </div>
+    )
+  }
+}
+
+function CardLoader() {
+  return (
+    <ContentLoader height={500} width={480} speed={1}>
+      <rect x="0" y="0" width="480" height="250" />
+      <rect x="65" y="280" rx="3" ry="3" width="350" height="60" /> 
+      <rect x="140" y="380" rx="30" ry="30" width="200" height="60" /> 
+    </ContentLoader>
+  )
+}
+
+class Body extends Component {
   state = {
     pokemonType: ['all', 'normal', 'fire', 'water', 'grass', 'flying', 'fighting', 'poison', 'electric', 'ground', 'rock', 'psychic', 'ice', 'bug', 'ghost', 'steel', 'dragon', 'dark', 'fairy'],
     tagCLick: 'all',
-    loadImg: 'loading',
   }
 
   activatedTag = async (e) => {
@@ -23,71 +64,6 @@ class PokemonCard extends Component {
     Store.fetchAllPokemon('https://pokeapi.co/api/v2/pokemon?limit=12')
   }
 
-  render() {
-    return (
-      <div>
-        <div className="custom-title w-5/6 sm:w-2/4 lg:w-1/3 mx-auto pt-6 border-b-2 border-red-600 text-center text-5xl text-yellow-500 font-black subpixel-antialiased">
-          POKEDEX APP
-        </div>
-        <div className="p-2 text-center text-lg text-gray-700 antialiased">
-          Select your favorite pokemon partner.
-        </div>
-    
-        <nav className="flex justify-center flex-wrap mt-4 mb-4 w-5/6 sm:w-2/4 lg:w-2/3 mx-auto text-white">
-          {this.state.pokemonType.map((type, index) => (
-            <div
-              key={index}
-              className={`w-auto ${this.state.tagCLick === type ? 'bg-red-600 text-white' : 'bg-gray-200 text-gray-700'} hover:bg-red-600 hover:text-white rounded-full px-3 py-1 text-sm font-semibold m-1 cursor-pointer`}
-              onClick={this.activatedTag}
-            >
-              #{type}
-            </div>
-          ))}
-        </nav>
-
-        <div className="flex justify-center flex-wrap lg:w-5/6 mx-auto p-10 py-0">
-          {Store.dataAllPokemon ? 
-            Store.dataAllPokemon.map((pokemon, index) => (
-              <div key={index} className="flex-initial w-48 rounded-lg overflow-hidden shadow-lg bg-white hover:bg-gray-200 m-5 cursor-pointer">
-                <div
-                  className={css`
-                    background-image: url(${pokemon.sprites.front_default});
-                    background-repeat: no-repeat;
-                    background-position: center; `
-                    + " w-48 h-32 mx-auto pt-3"
-                  }
-                />
-                <div className="px-4 py-0">
-                <div className="font-bold text-sm mb-2 text-center capitalize">{pokemon.name.replace('-', ' ')}</div>
-                </div>
-                <div className="px-4 py-3 pt-0 text-center">
-                  {pokemon.types.map((type, index) => (
-                    <span key={index} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-xs font-semibold text-gray-700 m-1">#{type.type.name}</span>
-                  ))}
-                </div>
-              </div>
-            ))
-            : <div/>
-          }
-        </div>
-      </div>
-    )
-  }
-}
-
-class CardLoader extends Component {
-  render() {
-    return (
-      <ContentLoader height={500} width={480} speed={1}>
-        <rect x="0" y="0" width="480" height="250" />
-        <rect x="65" y="280" rx="3" ry="3" width="350" height="60" /> 
-        <rect x="140" y="380" rx="30" ry="30" width="200" height="60" /> 
-      </ContentLoader>
-    )
-  }
-}
-
-class Body extends Component {
   componentDidMount = () => {
     Store.fetchAllPokemon('https://pokeapi.co/api/v2/pokemon?limit=8')
   }
@@ -97,7 +73,7 @@ class Body extends Component {
   }
 
   render() {
-    let numberLoading = [1, 2, 3, 4, ]
+    let numberLoading = [1, 2, 3, 4]
 
     return (
       <InfiniteScroll
@@ -121,6 +97,25 @@ class Body extends Component {
           </div>
         }
       >
+        <div className="custom-title w-5/6 sm:w-2/4 lg:w-1/3 mx-auto pt-6 border-b-2 border-red-600 text-center text-5xl text-yellow-500 font-black subpixel-antialiased">
+          POKEDEX APP
+        </div>
+        <div className="p-2 text-center text-lg text-gray-700 antialiased">
+          Select your favorite pokemon partner.
+        </div>
+    
+        <nav className="flex justify-center flex-wrap mt-4 mb-4 w-5/6 sm:w-2/4 lg:w-2/3 mx-auto text-white">
+          {this.state.pokemonType.map((type, index) => (
+            <div
+              key={index}
+              className={`w-auto ${this.state.tagCLick === type ? 'bg-red-600 text-white' : 'bg-gray-200 text-gray-700'} hover:bg-red-600 hover:text-white rounded-full px-3 py-1 text-sm font-semibold m-1 cursor-pointer`}
+              onClick={this.activatedTag}
+            >
+              #{type}
+            </div>
+          ))}
+        </nav>
+
         <PokemonCard/>
       </InfiniteScroll>
     )
