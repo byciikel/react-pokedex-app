6b20b32d9630e01b90335598f0375b6a0027e0e8
Bayu Utomo <by.ciikel@gmail.com>
2020-01-19 05:56:02 +0700

    Updates

---
+++ precache-manifest.c401e7392d21986decd667b93fe25a4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2984b332870d02a97f28ad3be8cbb121",
+    "url": "/react-pokedex-app/index.html"
+  },
+  {
+    "revision": "313d441e4421dbf5f290",
+    "url": "/react-pokedex-app/static/css/main.3954e0a2.chunk.css"
+  },
+  {
+    "revision": "01b455779045c978b463",
+    "url": "/react-pokedex-app/static/js/2.a0ddc9e8.chunk.js"
+  },
+  {
+    "revision": "0c08a48b164499df78c0607b14eb2c76",
+    "url": "/react-pokedex-app/static/js/2.a0ddc9e8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "313d441e4421dbf5f290",
+    "url": "/react-pokedex-app/static/js/main.e8d27753.chunk.js"
+  },
+  {
+    "revision": "2b4e21f84c41cbbbcb28",
+    "url": "/react-pokedex-app/static/js/runtime-main.4075d925.js"
+  }
+]);
